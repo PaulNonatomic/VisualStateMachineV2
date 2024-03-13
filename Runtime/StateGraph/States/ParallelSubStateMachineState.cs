096967aa602f12ea6000f0c54b323b5327a562d0
Paul Stamp <paul@nonatomic.co.uk>
2024-03-13 23:00:41 +0000

    Fixed recentering

---
+++ Runtime/StateGraph/States/ParallelSubStateMachineState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Nonatomic.VSM2.NodeGraph;
+
+namespace Nonatomic.VSM2.StateGraph.States
+{
+	public class ParallelSubStateMachineState : BaseParallelSubStateMachineState
+	{
+		[Transition]
+		public event Action OnComplete;
+		
+		protected override void HandleComplete(State state)
+		{
+			base.HandleComplete(state);
+			OnComplete?.Invoke();
+		}
+	}
+}
