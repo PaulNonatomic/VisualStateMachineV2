d73e880473e80da19724d2f5942e445e1581a876
Nonatomic Macbook Air <paul@mac.lan>
2024-06-30 09:27:27 +0100

    Added tool to save all StateMachineModels when the project is saved

---
+++ Editor/StateMachineModelSaver.cs
@@ -0,0 +1,32 @@
+namespace VisualStateMachine.Editor.Utils
+{
+	using UnityEditor;
+	using UnityEngine;
+	using Nonatomic.VSM2.StateGraph;
+
+	[InitializeOnLoad]
+	public static class StateMachineModelSaver
+	{
+		static StateMachineModelSaver()
+		{
+			EditorApplication.projectChanged += SaveAllScriptableObjects;
+		}
+
+		private static void SaveAllScriptableObjects()
+		{
+			var guids = AssetDatabase.FindAssets("t:StateMachineModel");
+			foreach (var guid in guids)
+			{
+				var path = AssetDatabase.GUIDToAssetPath(guid);
+				var stateMachineModel = AssetDatabase.LoadAssetAtPath<StateMachineModel>(path);
+				if (stateMachineModel != null)
+				{
+					Debug.Log($"Save: {stateMachineModel.name}");
+					EditorUtility.SetDirty(stateMachineModel);
+				}
+			}
+
+			AssetDatabase.SaveAssets();
+		}
+	}
+}
