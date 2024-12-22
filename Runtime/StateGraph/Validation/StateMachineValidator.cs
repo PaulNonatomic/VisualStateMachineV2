ca2b99237ab9dfa95ffa258e539b685902ebd003
Paul Stamp <paul@nonatomic.co.uk>
2024-12-22 15:37:10 +0000

    Added a method to migrate node ports

--- Runtime/StateGraph/Validation/StateMachineValidator.cs
+++ Runtime/StateGraph/Validation/StateMachineValidator.cs
@@ -15,6 +15,9 @@ namespace Nonatomic.VSM2.StateGraph.Validation
 		{
 			if (GuardUtils.GuardAgainstRuntimeOperation()) return;
 
+			MigrateNodePorts(stateMachineModel);
+			MigrateTransitions(stateMachineModel);
+			
 			ValidateNodes(stateMachineModel);
 			ValidateNodePorts(stateMachineModel);
 			ValidateTransitions(stateMachineModel);
@@ -68,8 +71,7 @@ namespace Nonatomic.VSM2.StateGraph.Validation
 				// Handle migration (Older state machines had an OnEnterState method)
 				if (transition.DestinationNodeId == "OnEnterState")
 				{
-					var n = stateMachineModel.Nodes.FirstOrDefault(node => node.Id == transition.DestinationNodeId);
-					if(n == null) transition.DestinationNodeId = "OnEnter";
+					transition.DestinationNodeId = "OnEnter";
 				}
 				
 				var originNode = stateMachineModel.Nodes.FirstOrDefault(node => node.Id == transition.OriginNodeId);
@@ -88,6 +90,46 @@ namespace Nonatomic.VSM2.StateGraph.Validation
 				GraphLog.LogWarning(stateMachineModel.TryRemoveTransition(transition)
 					? $"Removed invalid transition {transition}"
 					: $"Could not remove invalid transition {transition}");
+			}
+		}
+		
+		private static void MigrateTransitions(StateMachineModel stateMachineModel)
+		{
+			if (GuardUtils.GuardAgainstRuntimeOperation()) return;
+			
+			for (var index = stateMachineModel.Transitions.Count - 1; index >= 0; index--)
+			{
+				var transition = stateMachineModel.Transitions[index];
+				
+				// Handle migration (Older state machines had an OnEnterState method)
+				if (transition.DestinationNodeId == "OnEnterState")
+				{
+					transition.DestinationNodeId = "OnEnter";
+				}
+			}
+		}
+		
+		private static void MigrateNodePorts(StateMachineModel stateMachineModel)
+		{
+			if (GuardUtils.GuardAgainstRuntimeOperation()) return;
+
+			foreach (var node in stateMachineModel.Nodes)
+			{
+				foreach (var port in node.InputPorts)
+				{
+					if(port.Id == "OnEnterState")
+					{
+						port.Id = "OnEnter";
+					}
+				}
+
+				foreach (var port in node.OutputPorts)
+				{
+					if(port.Id == "OnEnterState")
+					{
+						port.Id = "OnEnter";
+					}
+				}
 			}
 		}
 	}
